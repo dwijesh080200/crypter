22d3991213ea045a788d57ca22074644541d2e4d
dwijesh080200 <61412314+dwijesh080200@users.noreply.github.com>
2020-09-20 22:06:19 +0400

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -9,6 +9,12 @@ mkdir toCrypt
 mkdir crypted
 mkdir toDecrypt
 mkdir decrypted
+python3 key.py
 
 cp /home/user/crypter-decrypter/service/crypt.service /etc/systemd/system/
 cp /home/user/crypter-decrypter/service/decrypt.service /etc/systemd/system/
+
+sudo systemctl start crypt.service
+sudo systemctl enable crypt.service
+sudo systemctl start decrypt.service
+sudo systemctl enable decrypt.service
